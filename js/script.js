24e054622408e810bbe22cf1e91f03286446036c
Ahmed Moharam <110923613+Ahmedmoharam22@users.noreply.github.com>
2023-08-07 16:27:42 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ $(document).ready(function(){
   arrows: false,
   });
 });
+
+
+let span = document.querySelector("#btn");
+
+window.onscroll = function(){
+  if( scrollY >= 1200){
+    btn.classList.add("show")
+  }else{
+    btn.classList.remove("show")
+  }
+};
+
+span.onclick = function(){
+  window.scrollTo({
+    top:0,
+    behavior:"smooth",
+  });
+}
